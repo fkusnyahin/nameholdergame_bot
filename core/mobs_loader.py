a9f1282f7c63ce5d4be2647b81a02c46e8ee4792
fkusnyahin <fkusnyahin@gmail.com>
2026-05-24 09:22:32 +0300

    add mobs_loader.py

---
+++ core/mobs_loader.py
@@ -0,0 +1,21 @@
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# ЗАМЕНИ НА СВОЁ ИМЯ ФАЙЛА
+CRED_FILE = "mob-bot-mobdatabase-ad563d57da55.json"
+
+def get_mobs_from_sheets():
+    try:
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            CRED_FILE,
+            ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+        )
+        client = gspread.authorize(creds)
+        sheet = client.open("MobStats").sheet1
+        rows = sheet.get_all_records()
+        return rows
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return []
